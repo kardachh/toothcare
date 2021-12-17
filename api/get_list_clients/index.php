3a4f55a045ec8c523a61b63bb326e303fcd7a9a0
Кардаков Денис Алексеевич <Denis.Kardakov@norbit.ru>
2021-12-17 23:34:35 +0300

    first commit

---
+++ api/get_list_clients/index.php
@@ -0,0 +1,16 @@
+<?php
+$dbconn = pg_connect("host=localhost port=1111 dbname=ToothCare user=kardachh password=") or die('Не удалось соединиться');
+
+$query = 'SELECT id, surname, first_name, second_name, gender, birthday, address FROM "toothCare".patients order by surname';
+
+$result = pg_query($dbconn, $query);
+if (!$result) {
+  echo "Произошла ошибка.\n";
+  exit;
+}
+
+$clients = pg_fetch_all($result);
+
+$responce = array('clients'=>$clients);
+
+echo json_encode($responce);
---
+++ clients/index.php
@@ -0,0 +1,49 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php");
+?>
+<div class="container-main container">
+    <div class="clients-screen-wrap">
+        <div class="top-buttons">
+            <button class="container button" onclick="window.location='../'">
+                <div>Назад</div>
+            </button>
+            <div class="container page-name">
+                <div>Пациенты</div>
+            </div>
+            <button class="container button">
+                <div>Добавить пациента</div>
+            </button>
+        </div>
+        <div class="search-zone">
+            <input />
+        </div>
+    </div>
+</div>
+
+<script>
+    const url = 'http://toothcare:8888/api/get_list_clients';
+
+    const getData= async ()=>{
+        try {
+            const response = await fetch(url, {
+                method: 'GET', // или 'PUT'
+                // body: JSON.stringify(data), // данные могут быть 'строкой' или {объектом}!
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const json = await response.json();
+            return json;
+        } catch (error) {
+            console.error('Ошибка:', error);
+            return null;
+        } 
+    }
+
+    const docReady= async()=>{
+        const clients = await getData();
+        console.log(clients)
+    }
+
+    docReady()
+</script>
