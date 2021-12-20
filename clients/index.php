40cb40ba46613b3a8a57b47f1f03389f91cdfe29
Кардаков Денис Алексеевич <Denis.Kardakov@norbit.ru>
2021-12-20 22:42:04 +0300

    modal

--- clients/index.php
+++ clients/index.php
@@ -15,15 +15,62 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php");
             </button>
         </div>
         <div class="search-zone">
-            <input />
+            <div class="search-wrap">
+                <input class="search-input" placeholder="Начните вводить пациента..." />
+                <img src="/../assets/search.svg">
+            </div>
+        </div>
+        <div class="clients-list">
+
+        </div>
+    </div>
+</div>
+<!-- <a class="open_modal" href="#open">Открыть окно</a>
+<div id="modal" class="modal bounceIn">
+    <div id="close_modal">+</div>
+    <div class="modal_txt">Текст в модальном окне</div>
+</div> -->
+<div class="modal" style="display: none;">
+    <div class="container container-main modal-window">
+        <div class="modal-close">
+            <!-- <img src=''/> -->
+            <div>
+                X
+            </div>
+        </div>
+        <div class='client-info'>
+            <div class='client-fio'>
+                <div class="surname"></div>
+                <div class="first-name"></div>
+                <div class="second-name"></div>
+            </div>
+            <div class='client-additional-info'>
+                <div>
+                    <div>Телефон: </div>
+                    <div>Пол: </div>
+                    <div>Дата рождения: </div>
+                </div>
+                <div style="text-align: end;">
+                    <div class="phone"></div>
+                    <div class="gender"></div>
+                    <div class="birthday"></div>
+                </div>
+            </div>
+        </div>
+        <div class="diagnoses">
+            <div class="diagnoses-title">Диагнозы:</div>
+            <div class="diagnoses-list">
+                
+            </div>
         </div>
     </div>
 </div>
 
 <script>
     const url = 'http://toothcare:8888/api/get_list_clients';
+    let clients = [];
 
-    const getData= async ()=>{
+    const getData = async () => {
         try {
             const response = await fetch(url, {
                 method: 'GET', // или 'PUT'
@@ -37,14 +84,54 @@ require_once($_SERVER['DOCUMENT_ROOT'] . "/header.php");
         } catch (error) {
             console.error('Ошибка:', error);
             return null;
-        } 
+        }
     }
 
-    const docReady= async()=>{
-        const clients = await getData().then((res)=>res.clients);
-        clients.forEach(element => {
-            console.table(element);
+    const renderClient = (client) => {
+        let element = document.createElement("div");
+        element.id = `client-${client.id}`
+        element.classList = 'container client';
+        element.innerHTML = `<div>${client.surname} ${client.first_name} ${client.second_name} </div> <div>${client.phone}</div>`
+        return element;
+    }
+
+    const toggleModal = () => {
+        $('.modal').toggle()
+    }
+
+    const changeModal = (client) => {
+        console.table(client)
+        $('.surname').replaceWith(`<div class = "surname">${client.surname}</div>`)
+        $('.first-name').replaceWith(`<div class = "first-name">${client.first_name}</div>`)
+        $('.second-name').replaceWith(`<div class = "second-name">${client.second_name}</div>`)
+        $('.phone').replaceWith(`<div class = "phone">${client.phone}</div>`)
+        $('.gender').replaceWith(`<div class = "gender">${client.gender}</div>`)
+        $('.birthday').replaceWith(`<div class = "birthday">${client.birthday}</div>`)
+    }
+
+    const setClients = (clients) => {
+        const parentList = $(".clients-list")
+        clients.forEach(client => {
+            $(parentList).append(renderClient(client))
         });
+    }
+
+    const addHandlers = () => {
+        $('.client').click((data) => {
+            const id = data.currentTarget.id.split('-')[1]
+            const client = clients.find((client) => client.id === id)
+            changeModal(client)
+            toggleModal()
+        });
+
+        $('.modal-close').click(toggleModal)
+    }
+
+    const docReady = async () => {
+        clients = await getData().then((res) => res.clients);
+        setClients(clients)
+        addHandlers()
+        // $('.client')[0].click()
     }
 
     docReady()
