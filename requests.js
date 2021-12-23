4d7612a71f30aa3a2a749e7cf64ce9728e56b459
Кардаков Денис Алексеевич <Denis.Kardakov@norbit.ru>
2021-12-23 16:01:09 +0300

    refactoring

---
+++ requests.js
@@ -0,0 +1,36 @@
+import { Settings } from "./../settings.js";
+const { url } = Settings();
+
+const useFetch = () => {
+  const request = (url, params = {}, method = "GET") => {
+    let options = {
+      method,
+    };
+    if ("GET" === method) {
+      url += "?" + new URLSearchParams(params).toString();
+    } else {
+      options.body = JSON.stringify(params);
+    }
+
+    return fetch(url, options).then((response) => response.json());
+  };
+
+  const getClientsData = () => {
+    return request(url.urlClients);
+  };
+
+  const getDiagnosesData = async (id) => {
+    return request(url.urlDiagnosesList, {
+      client: id,
+    });
+  };
+
+  return {
+    api: {
+      getClientsData,
+      getDiagnosesData,
+    },
+  };
+};
+
+export { useFetch };
