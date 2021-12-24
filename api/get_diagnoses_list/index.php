734b82da92e5354cff3f85db4ab73bf2f62ebba8
Кардаков Денис Алексеевич <Denis.Kardakov@norbit.ru>
2021-12-24 03:21:14 +0300

    add client modal window with api

--- api/get_diagnoses_list/index.php
+++ api/get_diagnoses_list/index.php
@@ -1,13 +1,11 @@
 <?php
 header('Content-Type: application/json');
-$get = $_GET;
 $id_patient = $_GET['client'];
 
 $dbconn = pg_connect("host=localhost port=1111 dbname=ToothCare user=kardachh password=") or die('Не удалось соединиться');
 
 $query = 'SELECT dl.id_patient , d."name" , dl.description FROM "toothCare".diagnoses_list dl inner join "toothCare".diagnoses d on dl.id_diagnos = d.id where dl.id_patient = '.$id_patient.';';
 
-$url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 $result = pg_query($dbconn, $query);
 if (!$result) {
   echo "Произошла ошибка.\n";
